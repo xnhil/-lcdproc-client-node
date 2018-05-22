a8be29d561f307c42436728c353bdec398b62f39
Ben Thomas <ben@nhil.net>
2018-05-22 22:08:45 +1000

    Extend LCDproc menu support

--- lcdproc-client.js
+++ lcdproc-client.js
@@ -1,85 +1,118 @@
-var net = require("net");
+
+var net = require('net');
 var events = require("events");
-var lodash = require("lodash");
+var sys = require("sys");
 
-var screenDefaults = {
-  backlight: "on",
-  heartbeat: "on",
-  priority: 2
-};
+function screen(name){
+  var self = this;
+  self.screen_name = name
+  this.socket.write("screen_add screen\n");
+  this.socket.write("screen_set screen name {screen}\n");
+  this.socket.write("screen_set screen heartbeat on\n");
+  this.socket.write("screen_set screen priority 2\n");
+  this.socket.write("screen_set screen backlight on\n");
+}
+function widget(name) {
+  this.socket.write("widget_add screen " + name + " string\n");
+}
+function widget_val(name, x, y,value) {
+  this.socket.write("widget_set screen " + name + " " + x + " " + y + " {" + value +"}\n");
+}
+function menu_item(menu, id, type, name, options) {
+	var option_string = '';
+	
+	for(var option in options) {
+		option_string += ' -' + option + ' ' + options[option];
+	}
+	
+	var cmd =  'menu_add_item {' + menu + '} ' + id + ' ' + type + ' {' + name + '}' + option_string + "\n";
+	console.log(cmd);
+	this.socket.write(cmd);
+}
+function menu_item_set(menu, id, options) {
+	var option_string = '';
+	
+	for(var option in options) {
+		option_string += ' -' + option + ' ' + options[option];
+	}
+	
+	var cmd = 'menu_set_item ' + menu + ' ' + id + option_string + "\n";
+	console.log(cmd);
+	this.socket.write(cmd);
+}
+function menu_set(menu) {
+	var cmd = 'menu_set_main {' + menu + '}' + "\n";
+	console.log(cmd);
+	this.socket.write(cmd);
+}
+function menu_goto(menu) {
+	var cmd = 'menu_goto {' + menu + '}' + "\n";
+	console.log(cmd);
+	this.socket.write(cmd);
+}
+function client_set(name) {
+	var cmd = 'client_set -name {' + name + '}' + "\n";
+	console.log(cmd);
+	this.socket.write(cmd);
+}
 
-var escapeString = function (str) {
-  var needsEscaping = typeof str === 'string' && str.indexOf(' ') !== -1;
-  return (needsEscaping) ? "{" + str + "}" : str;
-};
-
-var LCDClient = function (host, port) {
+function init() {
+  var self = this;
   this.socket = new net.Socket();
-  this.host = host;
-  this.port = port;
-};
-
-LCDClient.prototype.createScreen = function (name, options) {
-
-  var self = this;
-
-  options = lodash.extend({}, screenDefaults, options);
-  self.name = name || "default_screen";
-
-  self.socket.write("screen_add " + self.name + "\n");
-
-  Object.keys(options).forEach(function (key) {
-    self.writeSocket("screen_set", self.name, key, options[key]);
-  });
-
-}
-
-LCDClient.prototype.writeSocket = function () {
-  var args = Array.prototype.slice.call(arguments);
-  args = lodash.flatten(args);
-  args = lodash.map(args, escapeString);
-  this.socket.write(args.join(" ") + "\n");
-};
-
-LCDClient.prototype.addWidget = function (name, type) {
-  type = type || "string";
-  this.writeSocket("widget_add", this.name, name, type);
-}
-
-LCDClient.prototype.updateWidget = function (name) {
-  // Output strings must be wrapped in curly braces: "{string}".
-  var args = Array.prototype.slice.call(arguments, 1);
-  this.writeSocket("widget_set", this.name, name, args);
-}
-
-LCDClient.prototype.init = function () {
-
-  var self = this;
-
-  self.socket.connect(self.host, self.port, function () {
+  this.socket.connect(this.host, this.port, function() {
     this.write("hello\n");
-    self.emit("init");
+    self.emit('init');
   });
+    
 
-  self.socket.on("data", function (d) {
-    var data_str = d.toString();
-    var params = data_str.split(" ");
-    if (params[0] == "connect") {
-      for (var i = 1; i < params.length; i++) {
-        if (params[i - 1] === "wid") {
+  this.socket.on('data', function(d) {
+//	  console.log(d);
+    data_str = d.toString();
+	data = data_str.split("\n");
+//	  console.log(d,data_str);
+	for(data_str of data) {
+    params = data_str.split(' ');
+    if (params[0] == 'connect')
+    {
+      for (i=1;i< params.length;i++)
+      {
+        if (params[i - 1] == 'wid')
+        {
           self.width = params[i];
         }
-        if (params[i - 1] === "hgt") {
+        if (params[i - 1] == 'hgt')
+        {
           self.height = params[i];
         }
       }
-      self.writeSocket("client_set", "name", "{node-lcdproc}");
+      self.socket.write("client_set name {NODEJS}\n");
       self.emit('ready');
-    }
+	} else if (data_str.length > 0) {
+//		console.log([data_str[data_str.length-1], data_str[data_str.length-2], data_str[data_str.length-3], data_str[data_str.length-4], data_str[data_str.length-5]]);
+		self.emit('data', {response: params[0], raw: data_str, params: params});
+	}
+//		  console.log(params);
+	}
   });
-
 }
 
-LCDClient.prototype.__proto__ = events.EventEmitter.prototype;
+exports.LcdClient = function( p_host, p_port) {
+  this.width=0;
+  this.height= 0;
+  this.host= p_host;
+  this.port= p_port;
+  this.socket= null;
+  this.init= init;
+  this.screen = screen;
+  this.widget = widget;
+  this.widget_val = widget_val;
+  this.menu_item = menu_item;
+  this.menu_item_set = menu_item_set;
+  this.menu_set = menu_set;
+  this.menu_goto = menu_goto;
+  this.client_set = client_set;
+};
+sys.inherits(exports.LcdClient, events.EventEmitter);
 
-module.exports = LCDClient;
+
+
